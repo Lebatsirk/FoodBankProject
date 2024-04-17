dac1794649d5a5e8e172c63719c5043a5cf7e967
Kristabel <86485768+Lebatsirk@users.noreply.github.com>
2024-04-17 17:19:46 +0100

    Add config.php redacted (commit typo)
    Cannot change commit message already committed online so pushing again to note it and will be more careful in future!

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// scripts/config.php
+
+$host = "localhost";
+$user = "***REDACTED***";
+$password = "***REDACTED***";
+$db = "***REDACTED***";
+
+// echo("Loaded config for user " . $user . " with DB " . $db);
+?>
